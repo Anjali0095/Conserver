a2c1cb7f97bd4260f27605bde8dafebca364da27
Anjali <smoni6465@gmail.com>
2024-01-23 11:35:16 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Server live.....
+          ReactApp is live
         </a>
       </header>
     </div>
